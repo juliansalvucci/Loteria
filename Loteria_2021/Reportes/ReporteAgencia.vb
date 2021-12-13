c2318049dd74206f5ab20933564e254480f89bdf
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-12-13 19:56:00 -0300

    Reset

--- Loteria_2021/Reportes/ReporteAgencia.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class ReporteAgencia
-    Private Sub ReporteAgencia_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: esta línea de código carga datos en la tabla 'LoteriaDataSetAgencia.Reporte_Agencia' Puede moverla o quitarla según sea necesario.
-        Me.Reporte_AgenciaTableAdapter.Fill(Me.LoteriaDataSetAgencia.Reporte_Agencia)
-
-        Me.ReportViewer1.RefreshReport()
-    End Sub
-End Class
--- Loteria_2021/Reportes/ReporteUsuarios.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class ReporteUsuarios
-    Private Sub ReporteUsuarios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: esta línea de código carga datos en la tabla 'LoteriaDataSet.obtener_usuarios' Puede moverla o quitarla según sea necesario.
-        Me.obtener_usuariosTableAdapter.Fill(Me.LoteriaDataSet.obtener_usuarios)
-
-        Me.ReportViewer1.RefreshReport()
-    End Sub
-End Class
